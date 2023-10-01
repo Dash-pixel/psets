4bbb087a5801673f434d815c1c18ea63e6e74730
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-01 12:52:49 +0000

    Sun, Oct 1, 2023, 3:52 PM +03:00

--- population.c
+++ population.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio>
 
-int main(void){
 
-    string name = get_sting("wht ur name ");
-    int age = get_int("wht ur age ");
-    string phone = get_string("wht ur phone ");
+
+int main(void){
+    int starting_number = get_int("How many lammas at start ");
+    int ending_number = get_int("How many lammas at end ");
+
 
 }
