9c8fd80f98ee613adcd635f3338a3727969f0cac
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-01 13:13:26 +0000

    Sun, Oct 1, 2023, 4:13 PM +03:00

--- population.c
+++ population.c
@@ -7,10 +7,8 @@ int main(void){
     int starting_number = get_int("How many lammas at start ");
     int ending_number = get_int("How many lammas at end ");
 
-    int years_to_get_to_goal(starting_number){
-
-        starting_number
-    }
+    int years_to_get_to_goal = starting_number
+    
 printf(years_to_get_to_goal);
 
 }
