250b1a720105bb41f80b09b9a91c46fa492a32c9
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-02 19:47:22 +0000

    Mon, Oct 2, 2023, 10:47 PM +03:00

--- pop.c
+++ pop.c
@@ -6,10 +6,11 @@
 int main(void){
     int starting_number = get_int("How many lammas at start ");
     int ending_number = get_int("How many lammas at end ");
+    float
     int years;
 
     if (starting_number < ending_number){
-        starting_number = starting_number*13/12;
+        starting_number = starting_number + (1 / 12);
         years ++;
     }
 print(years)
