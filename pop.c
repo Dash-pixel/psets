0f94e6d9bb5ad173350b8855fe051da6d03cbbe7
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-02 19:39:47 +0000

    Mon, Oct 2, 2023, 10:39 PM +03:00

--- pop.c
+++ pop.c
@@ -6,7 +6,9 @@
 int main(void){
     float starting_number = get_float("How many lammas at start ");
     float ending_number = get_float("How many lammas at end ");
-    
-for
+
+    for (i = 0; i <){
+
+    }
 
 }
