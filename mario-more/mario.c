24ae8c745099da72e129454894274ba7aca9d821
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-06 14:34:11 +0000

    Fri, Oct 6, 2023, 5:34 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -12,8 +12,8 @@ int main(void)
 
     for (int i = 0; i < height; i++){
         for (int j = 0; j < i; j++){
-            print(" ");
-            print("#");
+            printf(" ");
+            printf("#");
 
         }
         printf("#\n");
