b839ac4e88a53bd71c73a7f022adecad70208bb1
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-06 17:46:12 +0000

    Fri, Oct 6, 2023, 8:46 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -10,7 +10,8 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    for (int i = 1; i < (height+1); i++){
+    for (int i = 1; i < (height+1); i++)
+    {
         int b = height - i;
         repeat_char(' ', height - i);
         repeat_char('#', i);
@@ -18,11 +19,7 @@ int main(void)
         repeat_char('#', i);
         printf("\n");
     }
-
 }
-
-//////////////////////////
-
 void repeat_char (char charecter, int number_of_times)
 {
     for (int j = 0; j < number_of_times; j++)
