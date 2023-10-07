26ae390aadf1fef5f613919dad30683bf501f209
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-07 19:09:29 +0000

    Sat, Oct 7, 2023, 10:09 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,6 +5,7 @@ int main(void)
 {
     long credit_number = get_long("Number:");
 
+// превратить все числа числа в отдельные диджиты
         for ( i = 1; i < 5; i++){
             = get_long % (10 * n)
         }
