0077d000d56a705930f6e5c3c61ceac28f7f62ae
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-08 17:46:34 +0000

    Sun, Oct 8, 2023, 8:46 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -28,7 +28,7 @@ int main(void)
     for (int i = 0; i < length; i++){
 // 222222
         even_numbers = card_number % (10*i) * 2;
-
+        card
         odd_numbers = card_number % (10*i) * 2;
 
         if (even_number > 9)
