7d1eec0eed15a2e757e5ba95cfb17eee340e58bf
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-08 19:35:00 +0000

    Sun, Oct 8, 2023, 10:35 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,12 +11,19 @@ int main(void)
     if ((340000000000000 <= credit_number && credit_number< 350000000000000) || (370000000000000 <= credit_number && credit_number < 380000000000000))
     {
         printf("AMEX\n");
+        length = 15;
     }
     else if (5100000000000000 <= credit_number && credit_number < 5600000000000000)
     {
-
+        printf("AMEX\n");
+        length = 15;
     }
-    else if ((4000000000000000 <= credit_number && credit_number < 5000000000000000) || (4000000000000 <= credit_number && credit_number < 5000000000000))
+    else if (4000000000000000 <= credit_number && credit_number < 5000000000000000)
+    {
+        printf("AMEX\n");
+        length = 15;
+    }
+    else if (4000000000000 <= credit_number && credit_number < 5000000000000)
     {
 
     }
