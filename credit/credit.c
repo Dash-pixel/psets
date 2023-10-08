685cef0193a33f2cd88e4f75349f5a11c3ba7f99
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-08 16:43:47 +0000

    Sun, Oct 8, 2023, 7:43 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,11 +3,9 @@
 
 int main(void)
 {
-//American Express cards have 15 digits, MasterCard has 16 digits,
-//and Visa has either 13 or 16 digits. These numbers are made up of regular digits like 0, 1, 2, and so on.
-
-// all American Express cards start with 34 or 37,
-// most MasterCard cards start with 51, 52, 53, 54, or 55, and all Visa cards start with 4.
+// American Express cards start with 34 or 37, /// American Express cards have 15 digits
+// MasterCard cards start with 51, 52, 53, 54, or 55, /// MasterCard has 16 digits,
+// Visa cards start with 4 /// Visa has either 13 or 16 digits.
     long credit_number = get_long("Number:");
 
     if
