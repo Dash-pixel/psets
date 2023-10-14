6d8f7fe3ed13834d311beadd347755df2dff8da6
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-14 20:09:12 +0000

    Sat, Oct 14, 2023, 11:09 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -42,7 +42,7 @@ int main(void)
         if (i % 2 == 0)
         {
             digit = credit_number % (poww(10, i));
-            printf("sasha is baka");
+            printf("%i", poww(10, i));
         }
         else
         {
