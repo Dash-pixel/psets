9a5d93b438dffced67217baf05abaf84f79d938c
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-19 18:34:05 +0000

    Thu, Oct 19, 2023, 9:34 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -30,7 +30,7 @@ int main(void)
     words ++;
 
     float L = (float) (100 * letters) / words;
-    float S = (float) (sentances) / (100 * words);
+    float S = (float) (100 * sentances) / (words);
 
     int index = (0.0588 * L) - (0.296 * S) - 15.8;
 
