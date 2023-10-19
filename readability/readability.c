e2333ca767a5ea8a6b55ddc7f5a0321837f12226
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-19 17:21:53 +0000

    Thu, Oct 19, 2023, 8:21 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -19,7 +19,7 @@ int main(void)
         {
             sentances;
         }
-        else if ((text[i] == 46) && (text[i] == 33))
+        else if (((text[i] == 46) && (text[i] == 33)) || ((text[i] == 46) && (text[i] == 33)))
         {
 
         }
