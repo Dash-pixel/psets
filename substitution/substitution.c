63ffde7dab7f1bc4bbe5814bd7fae1470543a80b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-21 15:34:05 +0000

    Sat, Oct 21, 2023, 6:34 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,9 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <cs50.c>
 
 int main(int argc, string argv[])
 {
-
+ string code = string argv[1];
+ 
 }
 
 // как идея можно использовать разницу чисел
