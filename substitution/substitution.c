6f589d9142b305e7bda783ca1006a2466a98d724
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-10-21 15:08:28 +0000

    Sat, Oct 21, 2023, 6:08 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
 
 }
+
+// как идея можно использовать разницу чисел
+// а код замены можно сразу to_lowercase
+// решить задачу математически короч
