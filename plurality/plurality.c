6a36bd3d66fcbb65f6ba35545ac003b4b1f3df4f
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-11-10 17:20:42 +0000

    Fri, Nov 10, 2023, 7:20 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -82,9 +82,9 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > winner.votes)
+        if (candidates[i].votes > winner_votes)
         {
-            winner.name = candidates[i].name;
+            winner_votes = candidates[i].votes;
         }
     }
 
