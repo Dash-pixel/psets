3f3ae571cdaea7e9a662771f35783d9ef005a40d
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-11-10 16:34:05 +0000

    Fri, Nov 10, 2023, 6:34 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,8 +65,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    if (name == 
-    return false;
+    
 }
 
 // Print the winner (or winners) of the election
