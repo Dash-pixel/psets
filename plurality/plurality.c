8a9e743cc9b4f1ab244fbf225b81593336f0a368
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-11-10 17:01:23 +0000

    Fri, Nov 10, 2023, 7:01 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,16 +83,3 @@ void print_winner(void)
     return;
 }
 
-
-
-
-//// if /// candidate[i].name == "a"
-// candidate[i].vote ++;
-/*
-Complete the vote function.
-vote takes a single argument, a string called name, representing the name of the candidate who was voted for.
-If name matches one of the names of the candidates in the election, then update that candidate’s vote total to account for the new vote. The vote function in this case should return true to indicate a successful ballot.
-If name does not match the name of any of the candidates in the election, no vote totals should change, and the vote function should return false to indicate an invalid ballot.
-You may assume that no two candidates will have the same name.
-*/
-// 00000000 --
