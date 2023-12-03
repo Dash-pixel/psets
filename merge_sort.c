ad3df31c279a3f2f7c787dc18807a898ce37c987
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-03 17:50:11 +0000

    Sun, Dec 3, 2023, 7:50 PM +02:00

--- merge_sort.c
+++ merge_sort.c
@@ -1,20 +1,21 @@
 
 array1[1 3 5 6 7 8 2 4 9];
 array2[1 3 - 5 6 - 6 7 - 2 8 - 4 9];
-a = 0;
 
-function(array_length)
+
+function(Left_start; Right_end)
 {
     if array_length = 1
     {
         return;
     }
 
-    function(array_length/2);
+    function(Left_start; );
+
 
     Right_start = Left_start + (array_length / 2) + 1
 
-    for(int i = a(начальная); i < (array_length + a начальная); i++)
+    for(int i = Left_start; i < (array_length + Left_start); i++)
     {
 
         if a = b (начальная) or b будет в конце отрезка {
