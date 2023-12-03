a1cef638801bb47c2cb147f46d6bbf2eb7d4a3d8
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-03 16:19:51 +0000

    Sun, Dec 3, 2023, 6:19 PM +02:00

--- merge_sort.c
+++ merge_sort.c
@@ -1,6 +1,7 @@
 
 array1[1 3 5 6 7 8 2 4 9];
 array2[];
+a = 0;
 
 function(array_length)
 {
@@ -12,7 +13,6 @@ function(array_length)
 
     function(array_length/2);
 
-    a = 0;
     b = a + (array_length / 2) + 1
 
     for(int i = 0; i < array_length; i++)
