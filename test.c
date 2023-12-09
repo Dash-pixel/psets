1d33465681df949fab759d3fd15f57132c45036d
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-09 19:13:43 +0000

    Sat, Dec 9, 2023, 9:13 PM +02:00

--- test.c
+++ test.c
@@ -30,21 +30,6 @@ int main(void)
             Left++;
         }
         else
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        if ((Left != Right_start) && (Right != Left_start + array_length))
         {
 
             if(array[Left] < array[Right])
@@ -59,18 +44,6 @@ int main(void)
             Right++;
             }
         }
-        else if (Left == Right_start)
-        {
-            array2[i] = array[Right];
-            Right++;
-        }
-        else if (Right == Left_start + array_length)
-        {
-            array2[i] = array[Left];
-            Left++;
-        }
-         // if right is smaller than 6, else just increase left and write
-
     }
 
     for(int i = Left_start; i < Left_start + array_length; i++)
