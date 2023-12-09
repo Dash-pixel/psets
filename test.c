2e3c4eba0d78c2cdd07ddfa5dafb2f3edc02e451
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-09 13:28:36 +0000

    Sat, Dec 9, 2023, 3:28 PM +02:00

--- test.c
+++ test.c
@@ -4,8 +4,14 @@
 
 int main(void)
 {
-    int array[6];
-    int array[]
+    int array[6]{2, 5, 7, 3, 8, 9};
+    int array2[6];
+
+    Right_start = Left_start + (array_length / 2) + 1;
+
+    Left = Left_start;
+    Right = Right_start;
+
     int Leftstart
 
     for(int i = Left_start; i < Left_start + array_length; i++)
