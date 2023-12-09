835086f2109d94671e1846a06d75060012a19b7b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-09 13:19:02 +0000

    Sat, Dec 9, 2023, 3:19 PM +02:00

---
+++ array_test.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+    for(int i = Left_start; i < Left_start + array_length; i++)
+    {
+
+        if (Left != Right_start)
+        {
+
+            if(array[Left] < array[Right])
+            {
+            array2[i] = array[Left];
+            Left++;
+            }
+
+            else if(array[Left] > array[Right])
+            {
+            array2[i] = array[Right];
+            Right++;
+            }
+        }
+        else
+        {
+            array2[i] = array[Right];
+            Right++;
+        }
+
+    }
