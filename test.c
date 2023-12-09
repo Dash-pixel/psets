ee46545a690dd0dac3a332a692d403f0edcb0c53
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-09 20:17:14 +0000

    Sat, Dec 9, 2023, 10:17 PM +02:00

--- test.c
+++ test.c
@@ -15,7 +15,7 @@ sorting_inside(Left_start; Right_end)
     {
         return;
     }
-
+ array_length =  array_length/2;
     int Right_start = Left_start + (array_length / 2);
 
     int Left = Left_start;
