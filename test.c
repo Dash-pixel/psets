f6cc01ca361c637e9183e2fedb35de0ba5ca2981
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-09 19:23:10 +0000

    Sat, Dec 9, 2023, 9:23 PM +02:00

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main(void)
 {
     int array_length = 13;
 
-    int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int array[] = {0, 0, 0};
     int array2[array_length];
 
     int Left_start = 0;
