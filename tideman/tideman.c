d10997c77151f758c9614ec63c1f161e82b7cc85
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-18 19:30:10 +0000

    Mon, Dec 18, 2023, 9:30 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -152,7 +152,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-
+[candidate_count * (candidate_count - 1) / 2]
     prefences[pairs[i].winner][pairs[i].loser]
 
     return;
