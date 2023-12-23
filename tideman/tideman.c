bf15e55c00708d2452a9b16d01ab7184a191cecc
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-23 16:51:50 +0000

    Sat, Dec 23, 2023, 6:51 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -181,7 +181,7 @@ void lock_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
     {
-    int visited[MAX] = {-1};{0, 2, 4, -1}
+    int visited[MAX] = {-1};
     int unvisited[MAX] = {-1};
 
 
