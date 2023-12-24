4f6a306e604ec7fdd64302c0f447ce675f7b3416
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-24 20:51:44 +0000

    Sun, Dec 24, 2023, 10:51 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -217,9 +217,8 @@ void print_winner(void)
 {
     for(i=0; i<candidate_count; i++)
     {
-        for(j=0; j<candidate_count; j++)
+        for(j=0; locked[j][i] == 1; j++)
         {
-            if (locked[j][i] == 0)
         }
     }
     return;
