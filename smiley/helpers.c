2927726251448fb287816f7d9d0376c9a1b26834
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2023-12-30 20:37:15 +0000

    Sat, Dec 30, 2023, 10:37 PM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,9 +2,16 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    image.rgbtBlue = 0x00;
-    image.rgbtGreen = 0xFF;
-    image.rgbtRed = 0xFF;
+    for(int i = 0; i < width; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtGreen = 0x00)
+            {
+                    image.rgbtGreen = 0xFF;
+                    image.rgbtRed = 0xFF;
+            }
+        }
+    }
 
-    
 }
