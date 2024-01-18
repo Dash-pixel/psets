7a227d49bf59653aeda58f9305b2b47e6ae84a8c
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-01-18 11:51:45 +0000

    Thu, Jan 18, 2024, 1:51 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -25,12 +25,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++) {
 
         for (int j = 0; j < width; j++) {
-
+            image[i][j]
         }
 
     }
 
-    
+
     return;
 }
 
