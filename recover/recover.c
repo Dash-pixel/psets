5b727b91421686d3be43e9db60ecac8886f7957e
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-01-22 19:51:57 +0000

    Mon, Jan 22, 2024, 9:51 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -13,4 +13,16 @@ int main(int argc, char *argv[])
 // need to write in the buffer the whole card.raw?
 
 // while 512 bytes are not jpeg header, write down a file
+
+    fclose(raw_pointer);
+
+// Write text to the file
+    fprintf(filePointer, "Hello, World!\n");
+
+    // Close the file to save the file data
+    fclose(filePointer);
+
+    printf("File has been written successfully.\n");
+
+    return 0;
 }
