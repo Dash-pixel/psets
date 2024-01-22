9eadfd3764ba38ec9824bb52feb3e915d948f0a8
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-01-22 18:04:36 +0000

    Mon, Jan 22, 2024, 8:04 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
     FILE *raw_pointer = fopen(argv[1], "r");
 
     while (fread (*buffer, sizeof(char), 512, raw_pointer) = sizeof(char))
+
+    if 
 // need to write in the buffer the whole card.raw?
 
 // while 512 bytes are not jpeg header, write down a file
