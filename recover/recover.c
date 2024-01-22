2b8add263759d7e009f9b8af12986c4ac7f128b1
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-01-22 21:11:55 +0000

    Mon, Jan 22, 2024, 11:11 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,10 +9,7 @@ int main(int argc, char *argv[])
 
     while (fread (*buffer, sizeof(char), 512, raw_pointer) = sizeof(char)) == 255?
 
- buffer[0] == 0xff
- buffer [1] == 0xd8
- buffer [2] == 0xff
- (buffer[3] & 0xf0) == 0xe0
+  if (buffer[0] == 0xff && buffer [1] == 0xd8 && buffer [2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
 
     fclose(raw_pointer);
 
