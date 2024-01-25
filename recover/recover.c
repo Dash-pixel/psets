1f2986264f35205137e8bcb3f52845e47ec77963
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-01-25 18:03:59 +0000

    Thu, Jan 25, 2024, 8:03 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,11 +12,12 @@ int main(int argc, char *argv[])
     {
       if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && buffer[1] == 0xff && ((buffer[2] & 0xf0) == 0xe0))
       {
+        fclose(previous file);
         FILE *img = fopen(filename, "w");
       }
       else
       {
-        fwrite(data, size, number, outptr);
+        fwrite(filename, size, number, outptr);
       }
 
     }
