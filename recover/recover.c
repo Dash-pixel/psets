f24a81533247a7b5ab15dcb565c3624cf6e3eb99
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-01-25 20:28:23 +0000

    Thu, Jan 25, 2024, 10:28 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
         i++;
         sprintf(filename, "%03i.jpg", i);
         FILE *img = fopen(filename, "w");
-        fwrite(*buffer, sizeof(char), 512, //outptr);
+        fwrite(*buffer, sizeof(char), 512, filename);
       }
       else if(i > 0)
       {
-        fwrite(*buffer, sizeof(char), 512, //outptr);
+        fwrite(*buffer, sizeof(char), 512, filename);
       }
     }
 }
