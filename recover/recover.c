9b398d53d01964910997c8bdcb350bc14af9a560
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-14 13:16:57 +0000

    Wed, Feb 14, 2024, 3:16 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
     while(fread(buffer, sizeof(char), 512, raw_pointer) == 512)
     {
       if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && buffer[1] == 0xff && ((buffer[2] & 0xf0) == 0xe0))
+      //if the first block of array is the starting point of a jpeg file
       {
         if (i != 0) // why null?
         {
