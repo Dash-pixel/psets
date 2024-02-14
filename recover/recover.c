2fb8d34507d9855906a454d677ca13692ba18405
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-14 13:34:47 +0000

    Wed, Feb 14, 2024, 3:34 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,6 +14,12 @@ int main(int argc, char *argv[])
     if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[1] == 0xff) && ((buffer[2] & 0xf0) == 0xe0))
     {
       //the case where we start a new jpeg
+      if (i != 0) //we need to first close the old jpeg, if it exists
+
+        {
+          fclose(filename);
+        }
+
       FILE *img = fopen(filename, "w");
       fwrite(*buffer, sizeof(char), 512, *img);
     }
