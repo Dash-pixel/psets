6a15a0cca44c650301636bc316c494819bf0dde3
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-14 13:28:41 +0000

    Wed, Feb 14, 2024, 3:28 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,29 +3,27 @@
 
 int main(int argc, char *argv[])
 {
-    FILE *raw_pointer = fopen(argv[1], "r");
-
-    char buffer[512];
-    // just declaring how many elements in buffer, fread utterates through by itself
-    int i = 0;
-    char *filename = malloc(sizeof(char) * 8); // why this needed?
+  FILE *raw_pointer = fopen(argv[1], "r");
+  char *filename = malloc(sizeof(char) * 8); // why this needed? NEED TO WRITE FILE NAME IN STRING
+  char buffer[512]; // just declaring how many elements in buffer, fread utterates through by itself
+  int i = 0;
 
 
-    while(fread(buffer, sizeof(char), 512, raw_pointer) == 512)
+  while(fread(buffer, sizeof(char), 512, raw_pointer) == 512)
+  {
+    if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[1] == 0xff) && ((buffer[2] & 0xf0) == 0xe0))
     {
-      if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[1] == 0xff) && ((buffer[2] & 0xf0) == 0xe0))
-      {
-        //the case where we start a new jpeg
-      }
-      else if
-      {
-        //the case where we continue to write up the files
-      }
-      else
-      {
-        //the first case where we did not yet find a jpeg
-      }
-
+      //the case where we start a new jpeg
     }
+    else if
+    {
+      //the case where we continue to write up the files
+    }
+    else
+    {
+      //the first case where we did not yet find a jpeg
+    }
+
+  }
 }
 
