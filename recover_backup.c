580cc08469bf4c81a3030fbd6c754eba73ef8711
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-14 13:55:06 +0000

    Wed, Feb 14, 2024, 3:55 PM +02:00

--- recover_backup.c
+++ recover_backup.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
       else if(i > 0)
       {
-        fwrite(*buffer, sizeof(char), 512, filename);
+        fwrite(*buffer, sizeof(char), 512, *img);
       }
 
     }
