dd73ff5de3321cac6ec082efef34522b05dd305d
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-16 07:09:48 +0000

    Fri, Feb 16, 2024, 9:09 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -94,6 +94,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
             for (k = -1; k < 1; k++) {
                 for (l = -1; l < 1; l++) {
+                    
                     l * image[i + k][j + l]red
 
             // we need -1 0 1
