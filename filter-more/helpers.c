3817bb66b2a7cbbc260387b3c634477e04b14716
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-16 15:13:51 +0000

    Fri, Feb 16, 2024, 5:13 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -112,21 +112,21 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             for (int k = -1; k < 1; k++) {
                 for (int l = -1; l < 1; l++) {
 
-                    int multiplyer = (k == 0) ? 2 : 1);
+                    int multiplyer = (k == 0) ? 2 : 1;
 
                     if ((i + k > -1) && (j + l > -1) && (i + k < height) && (j + l < width))
                     {
 
-                        Gx_red += l * temp_image[i + k][j + l].rgbtRed * ((k == 0) ? 2 : 1);
+                        Gx_red += l * temp_image[i + k][j + l].rgbtRed * multiplyer;
                         Gx_green += l * temp_image[i + k][j + l].rgbtGreen * multiplyer;
-                        Gx_blue += l * temp_image[i + k][j + l].rgbtBlue * ((k == 0) ? 2 : 1);
+                        Gx_blue += l * temp_image[i + k][j + l].rgbtBlue * multiplyer;
                     }
 
                     if ((i + l > -1) && (j + k > -1) && (i + l < height) && (j + k < width))
                     {
-                        Gy_red += l * temp_image[i + l][j + k].rgbtRed * ((k == 0) ? 2 : 1);
-                        Gy_green += l * temp_image[i + l][j + k].rgbtGreen * ((k == 0) ? 2 : 1);
-                        Gy_blue += l * temp_image[i + l][j + k].rgbtBlue * ((k == 0) ? 2 : 1);
+                        Gy_red += l * temp_image[i + l][j + k].rgbtRed * multiplyer;
+                        Gy_green += l * temp_image[i + l][j + k].rgbtGreen * multiplyer;
+                        Gy_blue += l * temp_image[i + l][j + k].rgbtBlue * multiplyer;
                     }
                 }
             }
