0d4e61501a2b9f877e9e9048caac1773b02e1fd7
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-17 18:20:44 +0000

    Sat, Feb 17, 2024, 8:20 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,9 +53,10 @@ person *create_family(int generations)
         // TODO: Set parent pointers for current person
         this_person -> parents[0] = *parent0;
         this_person -> parents[1] = *parent1;
+        
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
-        
+
     }
  // If there are no generations left to create
     else
