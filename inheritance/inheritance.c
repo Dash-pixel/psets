32e009b63b54dffc82ec69bc23fb05f2a94c5427
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-17 18:20:07 +0000

    Sat, Feb 17, 2024, 8:20 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -55,6 +55,7 @@ person *create_family(int generations)
         this_person -> parents[1] = *parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
+        
     }
  // If there are no generations left to create
     else
