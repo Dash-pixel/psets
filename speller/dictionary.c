c3daecee31d53e5016bf17fb60418251069d5a1b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-18 19:30:51 +0000

    Sun, Feb 18, 2024, 9:30 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -35,7 +35,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    
+    fopen(*dictionary)
     return false;
 }
 
