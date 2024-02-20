a01d6c2129f09bb31fbb36338a47fc02ee7b6db3
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-20 22:09:18 +0000

    Tue, Feb 20, 2024, 10:09 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -59,7 +59,7 @@ unsigned int hash(const char *word)
 
     for (int i = 0; word[i] != '\0'; i++)
     {
-        summ += word[i] + i;
+        summ += word[i]*i; //maybe multiply here?? but is there a point 
     }
 
     int n = (1155*(summ % 2) + 385*(summ % 3) + 77*(summ % 5) + 11*(summ % 7) + (summ % 11));
