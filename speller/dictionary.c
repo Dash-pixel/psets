85a4032e4cc613c35d9b387907be9bd2d91d6746
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-21 14:37:36 +0000

    Wed, Feb 21, 2024, 4:37 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -56,13 +56,14 @@ unsigned int hash(const char *word)
     // return toupper(word[0]) - 'A';
     // 2*3*5*7*11
     unsigned int summ = 0;
+    char for_check
 
     for (int i = 0; word[i] != '\0'; i++)
     {
         summ += word[i] * (i + 1); //maybe multiply here?? but is there a point
         // how to make a trully unique identity??
         ///
-        printf("%c", word[i]);
+        for_check = word[i];
     }
 
     int n = (1155*(summ % 2) + 385*(summ % 3) + 77*(summ % 5) + 11*(summ % 7) + (summ % 11));
