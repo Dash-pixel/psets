2e240f46a0e55b6ecfb225805081b540180594cc
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-21 20:08:09 +0000

    Wed, Feb 21, 2024, 10:08 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -104,8 +104,9 @@ bool load(const char *dictionary)
 
     };
 /////////////////////////////////////////////////////////////////////////////
-  /*     for (int i = 0; i < N; i++) { // Iterate over each bucket
+       for (int i = 0; i < N; i++) { // Iterate over each bucket
         int count = 0; // Initialize node counter for the current bucket
+        double 
         node *cursor = table[i]; // Start with the first node in the bucket
 
         // Count nodes in the current linked list
@@ -116,7 +117,7 @@ bool load(const char *dictionary)
 
         printf("Bucket %d has %d nodes\n", i, count); // Print the count for this bucket
     }
-    */
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     fclose(dic);
