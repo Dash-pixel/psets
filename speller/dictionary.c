14451ad16b6aeed9c1acc3fc4f2237a5c558d9c0
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-21 06:33:16 +0000

    Wed, Feb 21, 2024, 8:33 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -64,7 +64,7 @@ unsigned int hash(const char *word)
         ///
     }
 
-    int n = (1155*(summ % 2) + 385*(summ % 3) + 77*(summ % 5) + 11*(summ % 7) + (summ % 11));
+    int n = 
     return n;
 
     //why is this a shit idea?
