9747ff0788924c7b287bcd6eacf8eafecfd39ab4
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-23 07:13:13 +0000

    Fri, Feb 23, 2024, 9:13 AM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open('2018m.csv', 'r') as
+    with open(sys.argv[1], 'r') as
         csv.DictWriter(file, fieldnames=["team", "rating"}])
 
     counts = {}
