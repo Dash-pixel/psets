fc3922191d5898d62fdba6b7dbac89905a43a081
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-23 21:30:22 +0000

    Fri, Feb 23, 2024, 11:30 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -14,7 +14,7 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
-    teams = [] # why is this an array
+    teams = [] # why is this a list
     # TODO: Read teams into memory from file
     f = open(sys.argv[1], 'r')
     reader = csv.DictReader(f, fieldnames=["team", "rating"])
