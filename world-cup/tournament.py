286fa5497de675f4db24a33207a91f2fd098f84b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-23 07:43:05 +0000

    Fri, Feb 23, 2024, 9:43 AM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -19,7 +19,7 @@ def main():
     f = open(sys.argv[1], 'r')
     reader = csv.DictWriter(f, fieldnames=["team", "rating"}])
     for row in reader:
-        teams
+        teams.append(row) 
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
