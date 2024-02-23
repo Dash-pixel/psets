53ae76564ee24db4a71e5ae5f34d8c994105b840
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-23 09:07:54 +0000

    Fri, Feb 23, 2024, 11:07 AM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -57,7 +57,8 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    # we should create a simulated 
+    # simulate round
+    # take only the winning teams
 
 
 if __name__ == "__main__":
