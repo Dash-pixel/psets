0a77b4c59be1af526f8367964b7f758d90ecf7ba
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-25 21:23:42 +0000

    Sun, Feb 25, 2024, 11:23 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,7 +1,5 @@
 # TODO
 '''first prompt the user with get_int for the half-pyramid’s height, a positive integer between 1 and 8, inclusive.
-(The height of the half-pyramids pictured above happens to be 4,
-the width of each half-pyramid 4, with a gap of size 2 separating them).
 
 If the user fails to provide a positive integer no greater than 8, you should re-prompt for the same again.
 
@@ -10,4 +8,3 @@ Then, generate (with the help of print and one or more loops) the desired half-p
 Take care to align the bottom-left corner of your pyramid with the left-hand edge of your terminal window,
 and ensure that there are two spaces between the two pyramids,
 and that there are no additional spaces after the last set of hashes on each row.'''
-
