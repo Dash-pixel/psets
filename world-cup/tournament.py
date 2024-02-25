c7af0b9e0f77b6582015f4a15706522a70be4a8c
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-25 14:07:11 +0000

    Sun, Feb 25, 2024, 4:07 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -25,7 +25,8 @@ def main():
 
     counts = {"team":"counts"}
     # TODO: Simulate N tournaments and keep track of win counts
-
+    # use simulate_tournament(teams)
+    
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
