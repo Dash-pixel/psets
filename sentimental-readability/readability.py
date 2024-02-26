9a042e7e626bb582d1cf36a57d1a0a15b8c1ab28
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 18:40:57 +0000

    Mon, Feb 26, 2024, 8:40 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -22,6 +22,6 @@ for i in text: # you can only iterate over a string (not a range) <-hide
 l = 100 * letters / words
 s = 100 * sentences / words
 
-index = (0.0588 * l) - (0.296 * s) - 15.8
+index = round((0.0588 * l) - (0.296 * s) - 15.8)
 
 print("Grade {}".format(index))
