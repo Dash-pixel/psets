7f92fcb1818b84e5816c751d2a8f666ad54c9e1f
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 13:32:14 +0000

    Mon, Feb 26, 2024, 3:32 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -10,7 +10,10 @@ digits = len(number)
 square_sum = 0
 check_sum = 0
 
-if
+if digits == 15 and number[0] == "3" and (number[1] == 4 or number[1] == 7):
+    card_type = "AMEX"
+elif digits == 16 and number[0] == "5" and (number[1] in range(1, 5)):
+    card_type = "MASTERCARD"
 
 for i in range(digits):
     if i % 2:
