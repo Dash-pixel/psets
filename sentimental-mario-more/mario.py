0fe5308dfa3b62b76bc15a3fb3c52423bfa77571
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 11:15:46 +0000

    Mon, Feb 26, 2024, 1:15 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -17,4 +17,4 @@ while (True):
     if height > 0 and height < 9:
         break
 for i in range(height):
-    print(" "*(height-i) + "#" * i + "  " + "#" * i)
+    print(" "*(height-i) + "#" * (i + 1) + "  " + "#" * (i + 1))
