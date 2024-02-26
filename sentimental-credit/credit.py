2f6ae2f552d047fd15ef32205646819abb419897
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 13:36:26 +0000

    Mon, Feb 26, 2024, 3:36 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,7 +3,7 @@ while (True):
         number = str(int(input('Number: ')))
         break
     except ValueError:
-        print("Invalid")
+        print("Invalid1")
         continue
 
 digits = len(number)
@@ -12,8 +12,13 @@ check_sum = 0
 
 if digits == 15 and number[0] == "3" and (number[1] == 4 or number[1] == 7):
     card_type = "AMEX"
-elif digits == 16 and number[0] == "5" and (number[1] in range(1, 5)):
+elif digits == 16 and number[0] == "5" and (number[1] in range(1, 6)):
     card_type = "MASTERCARD"
+elif (digits == 13 or 16) and (number[0] == 4):
+    card_type = "VISA"
+else:
+    print("Invalid2")
+
 
 for i in range(digits):
     if i % 2:
