2714f9810a345675ae2deda1273c90f2f95be362
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 17:23:06 +0000

    Mon, Feb 26, 2024, 7:23 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,10 +1,14 @@
 import cs50
 
-text = get_string("Text: ")
+text = cs50.get_string("Text: ")
+L = 0
+S = 0
 
+for i in text:
 
 
 
 index = 0.0588 * Letters - 0.296 * S(Sentances/100 words) - 15.8
 (words are spaces)
 
+print("Grade {}".format(index))
