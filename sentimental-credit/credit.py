a13cebf08cd9330d32b500f13776c8d56c6fa6ff
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 15:03:28 +0000

    Mon, Feb 26, 2024, 5:03 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -20,17 +20,15 @@ else:
     print("Invalid2")
 
 
-for i in range(digits): # итак, мы хотим начать не с первого а с последнего числа
-# [0, 1, 2, 3] - length is 4
-# i = 4 --> length - i
+for i in range(digits):
     index = digits - i
     if i % 2:
-        j = int(number[i])*2
+        j = int(number[index])*2
         if j > 9:
             j = j - 10 + 1
         square_sum += j
     else:
-        check_sum += int(number[i])
+        check_sum += int(number[index])
 
 check_sum += square_sum
 
