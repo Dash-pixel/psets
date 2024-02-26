972177b3011be633b7d247b2ff50e2bc46724dc0
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-26 13:42:06 +0000

    Mon, Feb 26, 2024, 3:42 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -10,11 +10,11 @@ digits = len(number)
 square_sum = 0
 check_sum = 0
 
-if digits == 15 and number[0] == "3" and (number[1] == 4 or number[1] == 7):
+if digits == 15 and number[0] == "3" and (number[1] == "4" or number[1] == "7"):
     card_type = "AMEX"
-elif digits == 16 and number[0] == "5" and (number[1] in range(1, 6)):
+elif digits == 16 and number[0] == "5" and (number[1] in str(range(1, 6))):
     card_type = "MASTERCARD"
-elif (digits == 13 or 16) and (number[0] == 4):
+elif (digits == 13 or 16) and (number[0] == "4"):
     card_type = "VISA"
 else:
     print("Invalid2")
