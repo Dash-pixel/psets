60cb4e734ee33d9d2cae04b7f2057a71bd9ac893
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-28 23:12:42 +0000

    Thu, Feb 29, 2024, 1:12 AM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -39,7 +39,7 @@ def main():  #<- how to use command-line args in python? 2 arguments
 
         if next_person == True:
             continue
-        print(i[name])
+        print(i['name'])
 
     return
 
