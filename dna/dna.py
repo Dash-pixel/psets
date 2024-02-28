5c4b72ab1999b595313f426ce551105e0c20b57a
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-28 19:27:29 +0000

    Wed, Feb 28, 2024, 9:27 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -30,8 +30,8 @@ def main(sys.argv):
 
     # TODO: Check database for matching profiles
 
-    for i in people:
-        for j in str_types[1:]
+    for i in people ():
+        for j in str_types[1:]:
             if people[i][j] != unknown_profile[j]:
                 break
         break
