1f1fbffd9d5c029f95447db7e12de399ce43a08b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-02-28 20:01:28 +0000

    Wed, Feb 28, 2024, 10:01 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -17,7 +17,7 @@ def main(sys.argv):
 
     # TODO: Read DNA sequence file into a variable
     #but i need to open multiple files?
-    with open('sys.argv[2], mode='r', encoding='utf-8') as file:
+    with open('sys.argv[2], mode='r') as file:
         dna = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
