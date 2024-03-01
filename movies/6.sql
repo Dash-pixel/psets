da89c2fd1317c84beac97b210e5d9ea2f320287c
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-01 20:07:07 +0000

    Fri, Mar 1, 2024, 10:07 PM +02:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,2 +1,2 @@
-SELECT SUM(rating)/ FROM ratings WITH movie_id
+SELECT SUM(rating)/ FROM ratings WITH movie_id = (SELECT)
 In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
