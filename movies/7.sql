4a25ef97ec658f1cf83f52bcf6edcca5fc14add9
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-01 21:13:07 +0000

    Fri, Mar 1, 2024, 11:13 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,4 +1,4 @@
 list all movies released in 2010 and their ratings, in descending order by rating.
 with the same rating, order them alphabetically by title.
 SELECT movies WHERE year = 2010
-INNER JOIN ratings ON
+INNER JOIN ratings ON movies.id = ratings.movie_id
