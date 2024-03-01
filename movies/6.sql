66fcfcf4d583faee45a37921af849a1ee9ee4cea
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-01 20:12:37 +0000

    Fri, Mar 1, 2024, 10:12 PM +02:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,2 +1,2 @@
-SELECT SUM(rating)/ FROM ratings WITH movie_id = (SELECT id FROM movies WITH)
+SELECT SUM(rating)/ COUNT(rating) FROM ratings WITH movie_id = (SELECT id FROM movies WITH year = 2012)
 In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
