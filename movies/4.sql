624d06893c3dce1c3156e96fab839bdd8c28bc03
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-01 08:11:18 +0000

    Fri, Mar 1, 2024, 10:11 AM +02:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,3 @@
 SELECT COUNT(*) FROM movies WHERE id = (SELECT movie_id FROM ratings WHERE rating = 10);
+SELECT title FROM movies WHERE id = (SELECT movie_id FROM ratings WHERE rating = 10);
+SELECT title FROM movies WHERE id = (SELECT movie_id FROM ratings WHERE rating = 10);
