faa70d044d5da21e855748f897da76853a9f3f44
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-01 21:47:34 +0000

    Fri, Mar 1, 2024, 11:47 PM +02:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,5 @@
 list the names of all people who starred in Toy Story.
 Your query should output a table with a single column for the name of each person.
 You may assume that there is only one movie in the database with the title Toy Story.
-SELECT
+SELECT name FROM people
+WHERE
