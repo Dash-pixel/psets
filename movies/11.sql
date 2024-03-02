e611dee35fd55957f933983af53e876875205f4f
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 07:17:12 +0000

    Sat, Mar 2, 2024, 9:17 AM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,4 +2,5 @@ list the titles of the five highest rated movies (in order) that Chadwick Bosema
 You may assume that there is only one person in the database with the name Chadwick Boseman.
 
 SELECT title FROM movies
-WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Chadwick Boseman'));
+WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN
+(SELECT id FROM people WHERE name = 'Chadwick Boseman'));
