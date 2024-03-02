e8c49e52137cbbb6d64608223989c807100d1266
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 08:26:27 +0000

    Sat, Mar 2, 2024, 10:26 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,3 @@
-In 13.sql, write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
-There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
+list the names of all people who starred in a movie in which Kevin Bacon also starred.
+Be sure to only select the Kevin Bacon born in 1958.
 Kevin Bacon himself should not be included in the resulting list.
