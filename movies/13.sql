c6cdd9fc352cc100480d02f45a593a64eeceff8a
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 09:28:43 +0000

    Sat, Mar 2, 2024, 11:28 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -7,6 +7,5 @@ FROM people JOIN stars ON people.id = stars.person_id
 WHERE stars.movie_id IN
 
 
-birth = 1958
 
-SELECT id WHERE name = 'Kevin Bacon' AND birth = 1958
+(SELECT id WHERE name = 'Kevin Bacon' AND birth = 1958)
