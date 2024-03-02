e624b8f010b917b9fd806881213978c08e69e0aa
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 23:04:11 +0000

    Sun, Mar 3, 2024, 1:04 AM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,9 +1,3 @@
-list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred
-
-SELECT title
-FROM movies JOIN stars ON movies.id = stars.movie_id
-WHERE stars.person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper');
-//
 SELECT title
 FROM movies
 WHERE id IN
