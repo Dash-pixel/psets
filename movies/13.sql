7bb2562a611676d2d8c997e5c938f60073871e0a
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 20:54:54 +0000

    Sat, Mar 2, 2024, 10:54 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,4 +2,4 @@ SELECT name
 FROM stars JOIN people ON stars.person_id = people.id
 WHERE stars.movie_id IN
 (SELECT stars.movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Kevin Bacon' AND birth = 1958)
-AND name != 'Kevin Bacon';.
+AND name != 'Kevin Bacon';
