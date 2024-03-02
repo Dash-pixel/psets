a9ed726c4a5ff7850c26962423d1639ec2308e6b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 09:31:19 +0000

    Sat, Mar 2, 2024, 11:31 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,10 +2,9 @@ list the names of all people who starred in a movie in which Kevin Bacon also st
 Be sure to only select the Kevin Bacon born in 1958.
 Kevin Bacon himself should not be included in the resulting list.
 SELECT name
-FROM
-FROM people JOIN stars ON people.id = stars.person_id
-WHERE stars.movie_id IN
-
+FROM stars JOIN people ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE
 
 
 (SELECT id WHERE name = 'Kevin Bacon' AND birth = 1958)
