0aa0e6e005dc1ab9379a50b6946f03902fe71c6d
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 20:43:33 +0000

    Sat, Mar 2, 2024, 10:43 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,7 +4,9 @@ Kevin Bacon himself should not be included in the resulting list.
 
 SELECT name
 FROM stars JOIN people ON stars.person_id = people.id
-WHERE stars.movie_id IN (SELECT stars.movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Kevin Bacon' AND birth = 1958);
+WHERE stars.movie_id IN
+(SELECT stars.movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Kevin Bacon' AND birth = 1958)
+AND name != 'Kevin Bacon';;
 
 
 
