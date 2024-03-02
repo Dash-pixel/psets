85a0033e158b3036a32cb4347d9fd66f50a0c6df
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 07:16:29 +0000

    Sat, Mar 2, 2024, 9:16 AM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,5 +1,5 @@
 list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
 You may assume that there is only one person in the database with the name Chadwick Boseman.
 
-SELECT title FROM movie
-WHERE id IN (SELECT title FROM movie WHERE id IN);
+SELECT title FROM movies
+WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Chadwick Boseman'));
