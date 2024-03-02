dd87c0615c15e809b426ca3769a9b32305be641d
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 22:54:21 +0000

    Sun, Mar 3, 2024, 12:54 AM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -7,7 +7,7 @@ WHERE stars.person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper');
 SELECT title
 FROM movies
 WHERE id IN
-(SELECT movie_id FROM stars JOIN people ON
+(SELECT movie_id FROM stars JOIN people ON stars.person_id = person.id WHERE name = 'Bradley Cooper' AND name = 'Jennifer Lawrence');
 
 WHERE name = 'Bradley Cooper');
 (SELECT id FROM people WHERE name = 'Bradley Cooper');
