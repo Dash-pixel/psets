e31bdceca34ab187c986fab174ae66d9a09549eb
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 06:34:24 +0000

    Sat, Mar 2, 2024, 8:34 AM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,6 +1,4 @@
 list the names of all people who starred in a movie released in 2004, ordered by birth year.
-output a single column with the name of each person.
-People with the same birth year may be listed in any order.
 No need to worry about people who have no birth year listed, so long as those who do have a birth year are listed in order.
 If a person appeared in more than one movie in 2004, they should only appear in your results once.
-SELECT name FROM people WHERE id = (SELECT person_id)
+SELECT UNIQUE name FROM people WHERE id = (SELECT person_id)
