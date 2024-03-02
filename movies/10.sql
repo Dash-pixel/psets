bded60bc95f7ae87a16758fec53bd326ef041e93
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 06:47:51 +0000

    Sat, Mar 2, 2024, 8:47 AM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -3,3 +3,8 @@ Your query should output a table with a single column for the name of each perso
 
 If a person directed more than one movie that received a rating of at least 9.0,
 they should only appear in your results once.
+
+SELECT DISTINCT name FROM people
+WHERE id IN (SELECT person_id FROM directors
+WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004))
+ORDER BY birth;
