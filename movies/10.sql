6806c444aec2f714b1231e3a2d06a193f85a6997
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-02 06:50:09 +0000

    Sat, Mar 2, 2024, 8:50 AM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -6,5 +6,4 @@ they should only appear in your results once.
 
 SELECT DISTINCT name FROM people
 WHERE id IN (SELECT person_id FROM directors
-WHERE movie_id IN (SELECT movie_id FROM ratings WHERE year = 2004))
-
+WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0))
