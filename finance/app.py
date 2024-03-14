a4f0dac1316cf3b75fb41be69ab69f9f5e1965a2
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-14 15:04:19 +0000

    Thu, Mar 14, 2024, 5:04 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -44,8 +44,10 @@ def buy():
     """Buy shares of stock"""
     #get request to buy stock
     if request.method == 'GET':
+        return render_template("buy.html")
+
+    else:
         
-    #return the
     return apology("TODO")
 
 
