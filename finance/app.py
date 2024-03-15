5fae105cdbc2f3850c0d5eefc1dcc9cd763556ed
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-15 19:58:43 +0000

    Fri, Mar 15, 2024, 9:58 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -62,7 +62,7 @@ def buy():
         else:
             new_cash = cash - to_pay
 
-            db.execute("SELECT cash FROM users WHERE id = ?", session.get('user_id'), cash)
+            db.execute("UPDATE ", session.get('user_id'), cash)
 
 
 @app.route("/history")
