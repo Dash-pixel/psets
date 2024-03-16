b73ecc1ed0c1b8ee03cb5b05ab25a48882819dc0
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-16 21:08:49 +0000

    Sat, Mar 16, 2024, 11:08 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -37,7 +37,7 @@ def index():
     """Show portfolio of stocks"""
     stock_list = db.execute('SELECT symbol, quantity FROM bought WHERE user_id = ?', session.get('user_id'))
     for i in stock_list:
-        i['current_price'] = (lookup(i['symbol'])).price
+        i['current_price'] = lookup(i['symbol']).price
         i['full_price']= i['current_price'] * int(i['quantity'])
 
     return render_template('index.html', stock_rows = stock_list)
