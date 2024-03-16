8e2115b71cd2b6c5fa7d992a360d0b5d23d87ee7
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-16 19:20:19 +0000

    Sat, Mar 16, 2024, 9:20 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -60,7 +60,7 @@ def buy():
 
         to_pay = stock_info['price'] * quantity
 
-        cash = db.execute("SELECT cash FROM users WHERE id = ?", session.get('user_id')) #what?
+        cash = db.execute("SELECT cash FROM users WHERE id = ?", session.get('user_id'))[0]['cash'] #what?
 
         if to_pay > cash:
             return apology("TODO")
@@ -71,6 +71,8 @@ def buy():
         affected_rows = db.execute("UPDATE bought SET quantity = quantity + ? WHERE user_id = ? AND symbol = ?", quantity, session.get('user_id'), symbol)
         if affected_rows == 0:
             db.execute("INSERT INTO bought (user_id, symbol, quantity) VALUES (?, ?, ?)", session.get('user_id'), symbol, quantity)
+
+        return redirect('/')
 
 @app.route("/history")
 @login_required
