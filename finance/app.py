6b76cb1a355015bb135a59eac9e22b5a4a21d078
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-16 11:16:07 +0000

    Sat, Mar 16, 2024, 1:16 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -41,6 +41,7 @@ def index():
 #how to do look up?
 #should i add price of stock to rows
 #or can i somehow dynamically ask from the index how much
+
     render_template('index.html', stock_rows, current_price)
     #return apology("TODO")
 
