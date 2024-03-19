d4138b2fee7d75d1a2b645c906dca943489da611
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-19 21:20:14 +0000

    Tue, Mar 19, 2024, 11:20 PM +02:00

--- _undone_binary search/binary_search.py
+++ _undone_binary search/binary_search.py
@@ -2,7 +2,27 @@
 arr =[6, 7, 10, 15, 19, 25, 40, 41, 45, 54, 65, 80, 93, 94, 98]
 x = 65
 
+
+
 def RecursiveSearch(arr, x, cursor):
+    #find the middle of the array
+
+    if arr[cursor] == x:
+        return cursor
+
+    elif arr[cursor] > x:
+        cursor = round(0.5 * cursor)
+
+    else:
+        cursor = round(1.5 * cursor)
+
+    RecursiveSearch(arr, x, cursor)
+
+RecursiveSearch(arr, x, (arr.length()/2))
+
+
+
+def RecursiveSearch(arr, x):
     #find the middle of the array
 
     if arr[cursor] == x:
