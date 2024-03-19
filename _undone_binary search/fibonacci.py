6c92045b2fcc7dbf4722fde43302de274c29d43b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-19 22:18:05 +0000

    Wed, Mar 20, 2024, 12:18 AM +02:00

--- _undone_binary search/fibonacci.py
+++ _undone_binary search/fibonacci.py
@@ -1,11 +1,15 @@
 def fibonacci(n):
     if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    else:
-        number = fibonacci(n-1) + fibonacci(n-2)
+        number = 1
         print(number)
+        return number
+    elif n == 1:
+        number = 1
+        print(number)
+        return number
+    else:
+        print(number)
+        number = fibonacci(n-1) + fibonacci(n-2)
         return number
 
 fibonacci(3)
