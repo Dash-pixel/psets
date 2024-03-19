44d1dcd5d2f6f53dc85803e3b218edbfb64680b2
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-19 20:57:18 +0000

    Tue, Mar 19, 2024, 10:57 PM +02:00

--- _undone_binary search/binary_search.py
+++ _undone_binary search/binary_search.py
@@ -2,13 +2,13 @@
 arr =[6, 7, 10, 15, 19, 25, 40, 41, 45, 54, 65, 80, 93, 94, 98]
 x = 65
 
-def RecursiveSearch(arr, x, arr.length):
+def RecursiveSearch(arr, x):
 
-    if arr[cursor] == x:
+    if arr[arr.length / 2]  == x:
         return
-    elif arr[cursor] > x:
-        new_cursor = cursor + (0.5 * cursor)
-    elif arr[cursor] < x:
-        new_cursor = cursor - (0.5 * cursor)
+    elif arr[arr.length / 2] > x:
+        
+    elif arr[arr.length / 2] < x:
 
-    RecursiveSearch(arr, x, new_cursor)
+
+    RecursiveSearch(arr, x)
