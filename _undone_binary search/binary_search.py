6e7235ab1a76c3019b9fe9e8051990bd7f92e79d
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-19 20:39:11 +0000

    Tue, Mar 19, 2024, 10:39 PM +02:00

--- _undone_binary search/binary_search.py
+++ _undone_binary search/binary_search.py
@@ -1,5 +1,7 @@
-arr = [33, 47, 12, 98, 59, 23, 77, 85, 66, 5, 39, 71, 88, 16, 45]
-x = 23 #Value to search for
+# Generate a sorted array of 15 random integers
+arr =[6,  7, 10, 15, 19, 25, 40, 41, 45, 54, 65, 80, 93, 94, 98]
+x = 65
 
 def RecursiveSearch(arr, x):
+    
 
