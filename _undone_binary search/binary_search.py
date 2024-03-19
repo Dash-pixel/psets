a633e8b440d92e91715195d84341866a3b5cca43
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-19 21:29:50 +0000

    Tue, Mar 19, 2024, 11:29 PM +02:00

--- _undone_binary search/binary_search.py
+++ _undone_binary search/binary_search.py
@@ -34,10 +34,10 @@ def RecursiveSearch(arr, x):
     elif arr[cursor] > x:
         cursor = round(0.5 * cursor)
         counter = 0.5 * array
-        RecursiveSearch(arr, x, cursor)
+        RecursiveSearch(arr, x)
     else:
         cursor = round(1.5 * cursor)
-        RecursiveSearch(arr, x, cursor)
+        RecursiveSearch(arr, x)
 
         counter +=1
 
