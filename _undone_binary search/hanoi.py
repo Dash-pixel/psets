59f6ee1b4c95ab8aaf278ae43da1a9d982f2bb17
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-20 09:22:13 +0000

    Wed, Mar 20, 2024, 11:22 AM +02:00

--- _undone_binary search/hanoi.py
+++ _undone_binary search/hanoi.py
@@ -6,3 +6,8 @@ rod_c = []
 
 def sorting():
      rod_c.append(rod_a[-1])
+
+
+#quite importaint that with tower of hanoi of 3 we want to place the first bagel on the futherest rod
+#but with 4 on the closesed
+# because odd and even - we want to finish on the futherest away
