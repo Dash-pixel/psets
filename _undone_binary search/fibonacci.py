a5f3893ab7a31823e0d9fcf9fc42041869d3dc94
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-21 12:48:35 +0000

    Thu, Mar 21, 2024, 2:48 PM +02:00

--- _undone_binary search/fibonacci.py
+++ _undone_binary search/fibonacci.py
@@ -16,12 +16,15 @@ fibonacci(10)
 how can we use dictionaries?
 we can check whether the value already exists in a dictionary
 """
-
+fib_dict = {}
 def fib(n):
     #check for number in dictionary
     #n is the oder/index in fibonacci
-    if n == 0:
+    if n in fib_dict:
+        return fib_dict[n]
+    elif n == 0:
         return 1
     elif n == 1:
         return 1
-    if
+    else:
+        return fib(n-1) + fib(n-2)
