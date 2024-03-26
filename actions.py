5243007feaf02ee9788414695511ff23ad1062f6
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-26 14:16:43 +0000

    Tue, Mar 26, 2024, 4:16 PM +02:00

--- actions.py
+++ actions.py
@@ -2,20 +2,33 @@ X = "X"
 O = "O"
 EMPTY = None
 
-board = [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+board = [[X, X, O],
+        [X, X, O],
+        [O, O, EMPTY]]
 
-def actions(board):
+def minimax(board):
     """
-    Returns set of all possible actions (i, j) available on the board.
-    return set of (i, j) (coordinates where something can be placed)
+    Returns the optimal action for the current player on the board.
     """
-    set_moves = set()
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == EMPTY:
-                set_moves.add((i, j))
-    return set_moves
 
-print(actions(board))
+    #case before last, but im doing utility in the wrong place and not calling terminal board
+    turn = X
+    possible_moves = {(3, 3)}
+    move_util_set = set() #also use set to ignore all the other results
+
+    for move in possible_moves:
+        new_board = [[X, X, O],
+                    [X, X, O],
+                    [O, O, X]]
+        if terminal(new_board):
+            score = 1
+        else:
+            score = minimax(new_board) #this is bs it returns the score.
+        move_util_set.add((move), score)
+
+    if turn == X:
+        bestmove = max(move_util_set, key=lambda x: x[1])
+        return bestmove[1]
+    if turn == O:
+        bestmove = min(move_util_set, key=lambda x: x[1])
+        return bestmove[1]
