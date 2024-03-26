beb86733b9437bb29fc2e4fff851bbd6fc2cfcb5
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-03-26 14:07:52 +0000

    Tue, Mar 26, 2024, 4:07 PM +02:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -54,6 +54,7 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    
     new_board = copy.deepcopy(board)
     i = action[0]
     j = action[1]
