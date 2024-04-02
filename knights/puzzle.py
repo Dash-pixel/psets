f2ce7b01a1e383d89c9b34651ae4df738698c44c
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-04-02 19:05:51 +0000

    Tue, Apr 2, 2024, 10:05 PM +03:00

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -34,8 +34,8 @@ knowledge1 = And(
 knowledge2 = And(
     Biconditional(AKnight, Not(AKnave)),
     Biconditional(BKnight, Not(BKnave)),
-    Implication(Or(And(AKnight, BKnight), And(Aknave, BKnave), AKnight),
-    Implication(Or(And(AKnight, BKnave), And(Aknave, BKnight), BKnight)
+    Implication(Or(And(AKnight, BKnight)), And((AKnave, BKnave), AKnight)),
+    Implication(Or(And(AKnight, BKnave)), And((AKnave, BKnight), BKnight))
 )
 
 # Puzzle 3
