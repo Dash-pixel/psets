0d7872a99147cd87e884a799138d8d46ea1d969b
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-04-04 14:40:52 +0000

    Thu, Apr 4, 2024, 5:40 PM +03:00

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -105,6 +105,7 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
+        self.cells
         
         raise NotImplementedError
 
