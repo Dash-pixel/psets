42b7bc3a9a81374665504881f54daac17c1657c6
Dash-pixel <69263577+Dash-pixel@users.noreply.github.com>
2024-04-05 15:19:08 +0000

    Fri, Apr 5, 2024, 6:19 PM +03:00

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -227,7 +227,8 @@ class MinesweeperAI():
 
         for old_sentence in self.knowledge: #how to impletement logic here?
             #we have to find the common part of the sentance
-            #
+            
+
 
     def make_safe_move(self):
         """
